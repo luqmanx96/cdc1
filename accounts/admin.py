8fba596439baf49b9e6ad30f23bf6c65b9106e59
Luqman Hakeem <luqmanx96@yahoo.com>
2021-04-06 17:55:14 +0800

    commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *  # * imports everything in the file
+
+admin.site.register(Customer)
+admin.site.register(Product)
