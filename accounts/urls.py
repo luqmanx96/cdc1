b24bf15039d660099f17f97da5b94e7681b0f6d3
Luqman Hakeem <luqmanx96@yahoo.com>
2021-04-08 17:29:34 +0800

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('customer/', views.customer, name="customer"),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+
     path('create_order/', views.createOrder, name='create_order'),
 ]
